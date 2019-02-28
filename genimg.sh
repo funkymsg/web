fc834c8199bd01724fc09409add2c1892adea053
Gilad Kutiel <gilad.kutiel@gmail.com>
2019-02-28 10:22:20 +0200

    form validation

--- genimg.sh
+++ genimg.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 for img in $(ls s/img); do
-    echo "<input type='radio' name='img' id='$img' value='$img'>"
+    echo "<input required type='radio' name='img' id='$img' value='$img'>"
     echo "<label for='$img'><img src='/img/$img' alt=''></label>"
 done
--- s/js/index.ts
+++ s/js/index.ts
@@ -7,21 +7,21 @@ document.addEventListener('DOMContentLoaded', () => {
         description: string
         msg: string
     }
-    
+
     const mobile = isMobile.isMobile()
     console.log('is mobile', mobile)
-    
+
     function getLongUrl(info: info) {
         return `${location.href}${encodeURIComponent(info.img)}/${encodeURIComponent(info.title)}/${encodeURIComponent(info.description)}/${encodeURIComponent(info.msg)}/`
     }
-    
+
     async function getShortUrl(info: info): Promise<string> {
         const res = await fetch(`https://avp.io/api/v2/action/shorten?key=ae34615cd0157f29c42263d132e7ef&url=${encodeURIComponent(getLongUrl(info))}`)
         return await res.text()
     }
-    
-    
-    (document.getElementById('get-link') as HTMLInputElement).addEventListener('click', async () => {
+
+
+    (document.getElementById('generator') as HTMLFormElement).addEventListener('submit', async (e) => {
         function getVal(id: string) {
             return (document.getElementById(id) as HTMLInputElement).value
         }
@@ -33,10 +33,12 @@ document.addEventListener('DOMContentLoaded', () => {
             msg: getVal('msg')
         }
 
+        e.preventDefault();
 
         const out = document.getElementById('short-url') as HTMLInputElement
         out.value = 'generating link...'
         out.value = await getShortUrl(info)
+        return false;
     })
 })
 
